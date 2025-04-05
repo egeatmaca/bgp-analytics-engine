e4f59c11d1753773ba10ee095e2081038fa5e075
egeatmaca <egeatmaca98@gmail.com>
2025-04-05 15:33:33 +0200

    spark data access

--- docker/spark/entrypoint.sh
+++ docker/spark/entrypoint.sh
@@ -3,7 +3,7 @@
 command=$1
 
 if [ $command = "start-master" ]; then
-  /opt/spark/sbin/start-master.sh
+  /opt/spark/sbin/start-master.sh -p $SPARK_MASTER_PORT
 elif [ $command = "start-worker" ]; then
   /opt/spark/sbin/start-worker.sh spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT
 elif [ $command = "start-history-server" ]; then
