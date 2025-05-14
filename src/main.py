4f4403f6a5da3a1ec391589ef70a3fc95ab0202f
egeatmaca <egeatmaca98@gmail.com>
2025-05-14 13:04:43 +0200

    pipeline updated

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from bgpstream_loader import BGPStreamLoader
 
 if __name__ == '__main__':
     loader = BGPStreamLoader(
-        collectors=['route-views.eqix'],
+        collectors=['rrc00'], # TODO: Set collector
         from_time="2024-07-07 00:00:00", 
-        until_time="2024-07-07 01:00:00"
+        until_time="2024-07-08 00:00:00"
     )
     loader.load_updates()
